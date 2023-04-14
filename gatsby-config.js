8069660af50a3cf8ba0202156a633d55305aacea
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-04-14 16:59:36 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+// /**
+//  * Configure your Gatsby site with this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+//  */
+
+// /**
+//  * @type {import('gatsby').GatsbyConfig}
+//  */
+module.exports = {
+  siteMetadata: {
+    title: `Static Blog`,
+    description: `This is example of static Gatsby blog`,
+    author: `@pwr`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-remark', 
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
